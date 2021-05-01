b149f99ac5931be45acd5f471417ff09b9db490b
b40deep@NettyzAir <boydtwine@gmail.com>
2021-05-01 17:04:21 +0300

    Implemented read-write to localStorage

--- src/components/Week.jsx
+++ src/components/Week.jsx
@@ -3,15 +3,13 @@ import Day from "./Day";
 class Week extends Component {
   constructor() {
     super();
-    this.state = {
-      Monday: ["mon1", "mon2"],
-      Tuesday: ["tue1", "tue2"],
-      Wednesday: ["wed1", "wed2"],
-      Thursday: ["thur1", "thur2"],
-      Friday: ["fri1", "fri2"],
-      Saturday: ["sat1", "sat2"],
-      Sunday: ["sun1", "sun2"],
-    };
+    this.state = {}
+  }
+
+  componentDidMount(){
+    //load saved task list from localStorage
+    this.handleReadFromStorage();
+    console.log('cdm successful');
   }
 
   handleCreateTask = (day, newTask) => {
@@ -22,31 +20,16 @@ class Week extends Component {
         let newTaskList = this.state[key];
         newTaskList.push(newTask);
         temp = Object.fromEntries(new Map([[day, newTaskList]]));
-        console.log("newTaskList>>", newTaskList);
-        console.log("temp>>", temp);
-        console.log("state>>", this.state);
+        // console.log("newTaskList>>", newTaskList);
+        // console.log("temp>>", temp);
+        // console.log("state>>", this.state);
       }
     }
     // setState
-    this.setState(temp);
+    this.handleWriteToStateAndStorage(temp);
     temp = [];
-    console.log("Week >> Task created successfully!");
+    console.log("WEEK >> Task created successfully!");
   };
-
-  // handleDeleteTask = (weekday, task) => {
-  //   console.log(`Task ${task} to be deleted from ${weekday} `);
-  //   let temp = Object.create({});
-  //   for (var key in this.state) {
-  //     if (key === weekday) {
-  //       let newTaskList = this.state[key].filter(listItem => listItem !== task);
-  //       temp = Object.fromEntries(new Map([[weekday, newTaskList]]));
-  //       console.log(`newTaskListTesting`, newTaskList);
-  //     }
-  //   }
-  //   //setState
-  //   this.setState(temp);
-  //   temp = [];
-  // }  
   
   handleDeleteTask = (weekday, task,taskKey) => {
     console.log(`Task ${task} with key ${taskKey} to be deleted from ${weekday} `);
@@ -54,14 +37,55 @@ class Week extends Component {
     for (var key in this.state) {
       if (key === weekday) {
         let newTaskList = this.state[key];
-        let taskToRemove = newTaskList.splice(taskKey,1);
+        newTaskList.splice(taskKey,1);
         temp = Object.fromEntries(new Map([[weekday, newTaskList]]));
         console.log(`newTaskListTesting`, newTaskList);
       }
     }
     //setState
-    this.setState(temp);
+    this.handleWriteToStateAndStorage(temp);
     temp = [];
+  }
+
+  handleWriteToStateAndStorage = (update) =>{
+    //set the state
+    //need to protect state from broken/empty update
+    if (update!==null) this.setState(update);
+
+    //then write changes to local storage
+    localStorage.setItem('state',JSON.stringify(this.state));
+  }
+
+  handleWriteToStateOnly = (update) =>{
+    //set the state
+    //need to protect state from broken/empty update
+    if (update!==null) this.setState(update);
+  }
+
+  handleReadFromStorage() {
+    let fromLocalStorage = JSON.parse(localStorage.getItem('state'));
+    if (fromLocalStorage!==null)this.handleWriteToStateOnly(fromLocalStorage);
+    // console.log('FLS________', fromLocalStorage);
+    console.log('ReadFromStorage successful');
+  }
+
+handleClearStorage() {
+    localStorage.clear();
+    console.log('ClearStorage successful');
+  }
+
+  tempInitStorage(){
+    const tempInit= {
+      Monday: ["mon1", "mon2"],
+      Tuesday: ["tue1", "tue2"],
+      Wednesday: ["wed1", "wed2"],
+      Thursday: ["thur1", "thur2"],
+      Friday: ["fri1", "fri2"],
+      Saturday: ["sat1", "sat2"],
+      Sunday: ["sun1", "sun2"],
+    };
+    localStorage.setItem('state',JSON.stringify(tempInit));
+    console.log('InitStorage successful');
   }
 
   render() {
@@ -69,7 +93,12 @@ class Week extends Component {
       <div className="col">
         {
           <React.Fragment>
-            <h1>Week</h1>
+<button className="btn btn-outline-info btn-sm m-2 btn-block" onClick={()=>{this.handleReadFromStorage()}}>Load Tasks</button>
+<button className="btn btn-outline-success btn-sm m-2 btn-block" onClick={()=>{this.tempInitStorage()}}>Save Tasks</button>
+<button className="btn btn-outline-warning btn-sm m-2 btn-block" onClick={()=>{this.handleClearStorage()}}>Clear Tasks</button>
+
+<h1>Week</h1>
+
             <ul className="list-group">
               {/* {console.log(Object.entries(this.state))} */}
 
@@ -80,6 +109,7 @@ class Week extends Component {
                     weekday={arrayPair[0]}
                     onCreateTask={this.handleCreateTask}
                     onDeleteTask={this.handleDeleteTask}
+                    onLoadTasks={this.handleReadFromStorage}
                   />
                 </li>
               ))}
